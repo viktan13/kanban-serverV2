e1fe20c5675dfe6c8fc1e16c888b116e77a6c34a
vikta <vik.tan@hotmail.com>
2022-12-10 11:08:15 -0600

    fix PORT

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const routes = require("./routes");
 const parseBody = require("./parseBody");
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 parseBody(app);
 routes(app);

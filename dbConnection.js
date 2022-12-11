997fcf421da06b78c5372f8a545ab5d86c26636c
vikta <vik.tan@hotmail.com>
2022-12-11 08:45:46 -0600

    created connection to remote MongoDB database

---
+++ dbConnection.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+
+const connectionString = 'mongodb+srv://admin:Z8GSBi5yIzGCMiwY@cluster0.umvyr8v.mongodb.net/?retryWrites=true&w=majority';
+const options = {
+    serverSelectionTimeoutMS: 5000, // Keep trying to send operations for 5 seconds
+    socketTimeoutMS: 45000, // Close sockets after 45 seconds of inactivity};
+    }
+
+function dbConnection(){
+    mongoose.set('strictQuery', true);
+ mongoose.connect(connectionString, options)
+     .then(() => console.log('Connected to MongoDB'))
+     .catch(err => console.log(err));
+}
+
+module.exports = dbConnection;
--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const routes = require("./routes");
 const parseBody = require("./parseBody");
+const dbConnection = require("./dbConnection");
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+dbConnection();
 parseBody(app);
 routes(app);
 

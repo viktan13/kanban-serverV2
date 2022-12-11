5c3cb4f74b25139af9b98e795e138156b4807a49
vikta <vik.tan@hotmail.com>
2022-12-11 09:06:49 -0600

    some fixes

--- routes.js
+++ routes.js
@@ -3,11 +3,11 @@ const cardGetAll = require("./card/cardGetAll");
 const cardUpdateById = require("./card/cardUpdateById");
 const cardDelete = require("./card/cardDelete");
 
-let cards = [
-    {id: '1', name: 'First Card', status: 'todo', priority: 2},
-    {id: '2', name: 'Second Card', status: 'progress', priority: 5},
-    {id: '3', name: 'Third Card', status: 'review', priority: 3},
-];
+// let cards = [
+//     {id: '1', name: 'First Card', status: 'todo', priority: 2},
+//     {id: '2', name: 'Second Card', status: 'progress', priority: 5},
+//     {id: '3', name: 'Third Card', status: 'review', priority: 3},
+// ];
 
 function routes(app) {
     app.get('/', (req, res) => {

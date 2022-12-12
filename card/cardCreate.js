70af39dd176048dcd1b22724ff5027cf6e19f112
Viktor <vik.tan@hotmail.com>
2022-12-11 22:02:00 -0600

    connected cardCreate controller to db

--- card/cardCreate.js
+++ card/cardCreate.js
@@ -1,7 +1,17 @@
+const Card = require("./Model");
+
 function cardCreate(req, res) {
     // const newCard = req.body;
     // cards.push({id: Math.random().toFixed(20).slice(2), ...newCard});
-    res.send('New card is created');
+    const newCard = new Card(req.body);
+    newCard.save()
+        .then(() => {
+            res.send('New card is created');
+        })
+        .catch(err => {
+            console.log(err);
+            res.send('New card was not created. Some Error')
+        })
 }
 
 module.exports = cardCreate;

932b564ff5b853f9cd5552eefc9c163a0c1509d1
Viktor <vik.tan@hotmail.com>
2022-12-11 22:45:02 -0600

    connected cardGetAll controller to db

--- card/cardDelete.js
+++ card/cardDelete.js
@@ -1,7 +1,15 @@
+const Card = require("./Model");
+
 function cardDelete(req, res) {
-    // const cardId = req.params.cardId;
-    // cards = cards.filter(el => el.id !== cardId);
-    res.send('Card is deleted')
+    const cardId = req.params.cardId;
+    Card.deleteOne({_id: cardId})
+        .then(() => {
+            res.send('Card is deleted')
+        })
+        .catch(err => {
+            console.log(err);
+            res.send('Card was not deleted. Some Error')
+        })
 }
 
 module.exports = cardDelete;

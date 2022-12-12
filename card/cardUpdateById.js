d02ff6d5d402883a383ebeddc5c4cddf7decd612
Viktor <vik.tan@hotmail.com>
2022-12-11 22:36:36 -0600

    connected cardGetAll controller to db

--- card/cardUpdateById.js
+++ card/cardUpdateById.js
@@ -1,8 +1,17 @@
+const Card = require("./Model");
+
 function cardUpdateById(req, res) {
-    // const cardId = req.params.cardId;
-    // const updCard = req.body;
-    // cards = cards.map(el => el.id === cardId ? {...el, ...updCard} : el);
-    res.send('Card is updated');
+    const cardId = req.params.cardId;
+    const filter = {_id: cardId};
+    const updCard = req.body;
+    Card.updateOne(filter, updCard)
+        .then(() => {
+            res.send('Card is updated');
+        })
+        .catch(err => {
+            console.log(err);
+            res.send('Card was not updated. Some Error')
+        });
 }
 
 module.exports = cardUpdateById;
